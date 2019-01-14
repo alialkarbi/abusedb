d528ac57e305e6227ec319f83fc7bc870b4cca03
alialkarbi <30644378+alialkarbi@users.noreply.github.com>
2019-01-14 12:45:33 +0300

    Create abusedb.py

---
+++ abusedb.py
@@ -0,0 +1,33 @@
+#####
+# Abuse-DB checking script
+# Ali Alkarbi
+# am.alkarbi@gmail.com
+#
+####
+import requests
+import json
+import sys
+
+fileName = sys.argv[1]
+headers = {
+    'Key': 'Your Key',
+    'Accept': 'application/json'
+}
+with open(fileName) as ipList:
+	for ipAddress in ipList.readlines():
+		parameters = {"ipAddress": ipAddress,'maxAgeInDays': '1','verbose': ''}
+		response = requests.get('https://api.abuseipdb.com/api/v2/check', params=parameters, headers=headers)
+		rawData = json.loads(response.content.decode("utf-8"))
+		country = rawData['data']['countryName']
+		totalReports = rawData['data']['totalReports']
+		isWhitelisted=str(rawData['data']['isWhitelisted'])
+		if isWhitelisted == "False":
+			isWhitelisted = "No"
+		elif isWhitelisted == "True":
+			isWhitelisted = "Yes"	
+		elif isWhitelisted == "None":
+			isWhitelisted = "No Information"
+		abuseConfidenceScore=rawData['data']['abuseConfidenceScore']
+		
+		print ("IP-Address: " ,ipAddress.strip(), "| Country: ", country, "| Total-Reports: ", totalReports,"| White-List: ",isWhitelisted,"| Abuse-Score: ",abuseConfidenceScore)
+		
